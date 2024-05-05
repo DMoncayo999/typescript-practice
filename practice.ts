418db2e75e67d089d940d7e0b9ad1f1583d91b24
DMoncayo999 <delfina.moncayo@gmail.com>
2024-05-04 20:50:41 -0400

    Add async/await function to complete requirements

--- practice.ts
+++ practice.ts
@@ -13,3 +13,30 @@ function factorial(n: number): number {
 }
 const result = factorial(4);
 console.log("Factorial of 4:", result); 
+
+
+// Asynchronous function using async/await
+function washDish(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dishWashed = true;
+            if (dishWashed){
+                resolve("You washed the dishes");
+            }
+            else{
+                reject("You did not wash the dishes");
+            }
+        }, 1000);
+    });
+}
+
+async function doTask() {
+    try {
+        const washDishResult = await washDish();
+        console.log(washDishResult);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+doTask();
